6b44e36ef5586dc409771e6ce0bf7f17f3d92965
Tommydreamer57 <minilao94@yahoo.com>
2018-04-25 12:10:17 -0600

    it works!

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -5,12 +5,31 @@ export default class Results extends Component {
         super();
     }
     componentDidMount() {
-
+        let { gameid } = this.props.match.params;
+        if (!this.props.joinedGame.board.length) {
+            this.props.socket
+        }
     }
     render() {
+        let { joinedGame } = this.props;
+        let { _id, players, user, date, startTime, endTime, winner } = joinedGame;
+        console.log(this.props);
         return (
             <div className="Results">
-                {JSON.stringify(this.props)}
+                <h1>{_id}</h1>
+                {
+                    players.sort((a, b) => {
+                        if (a.points !== b.points) return a.points < b.points;
+                        else return a.words.length < b.words.length
+                    }).map(player => {
+                        return (
+                            <div className={'player ' + player.winner ? 'winner' : ''} >
+                                <h2>{player.name} - {player.points}</h2>
+                                {player.words.join(' ')}
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
